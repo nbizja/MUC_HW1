cda2abf4676ff53e6194d58b123340a5518e40ad
Nejc Bizjak <neco.bizjak@gmail.com>
2015-10-24 22:53:58 +0200

    Fixed problem with nested fragments. Map is created programatically now.

--- app/src/main/java/nb7232/muc_hw1/ConnectionMapFragment.java
+++ app/src/main/java/nb7232/muc_hw1/ConnectionMapFragment.java
@@ -2,27 +2,24 @@ package nb7232.muc_hw1;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
 
-public class ConnectionMapFragment extends Fragment {
+public class ConnectionMapFragment extends SupportMapFragment {
 
-    protected GoogleMap map;
+    public ConnectionMapFragment() {
+        super();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.connection_map_fragment, container, false);
-        if (map == null) {
-            map = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.connection_map))
-                    .getMap();
-        }
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        getMap();
 
         return view;
     }
